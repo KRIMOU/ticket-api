57c133538d560eea5cdf3892d783d94d0aea6c18
KRIMOU <krimocbe@gmail.com>
2025-08-12 17:49:38 +0200

    Add Unit Tests while isoliting tests

--- src/main/java/com/pluralsight/ticket_api/DAO/jdbc/PersonRepository.java
+++ src/main/java/com/pluralsight/ticket_api/DAO/jdbc/PersonRepository.java
@@ -9,12 +9,25 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.List;
 
 @Repository
 public class PersonRepository {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
+
+    public int deleteById(int id) {
+        return jdbcTemplate.update("delete from person where id=?", id);
+    }
+
+    public int insertPerson(Person person) {
+        Object[] args = {person.getId(), person.getName(), person.getLocation(), Timestamp.from(Instant.now())};
+
+        return jdbcTemplate.update("insert into person (id, name, location, birth_date) " + "values(?,  ?, ?, ?)", args);
+    }
 
     static class PersonRowMapper implements RowMapper<Person> {
         @Override
@@ -27,6 +40,11 @@ public class PersonRepository {
             return person;
         }
 
+    }
+
+
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person", new PersonRowMapper());
     }
 
     public Person findById(int id) {
--- src/test/java/com/pluralsight/ticket_api/repository/PersonRepositoryTest.java
+++ src/test/java/com/pluralsight/ticket_api/repository/PersonRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.pluralsight.ticket_api.repository;
 
 import com.pluralsight.ticket_api.DAO.jdbc.PersonRepository;
+import com.pluralsight.ticket_api.model.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,29 @@ public class PersonRepositoryTest {
     @Test
     void givenPersonId_whenFetchPerson_thenReturnPerson(){
         assertNotNull(personRepository);
-        assertEquals(personRepository.findById(1).getName(),"John Doe");
+        assertEquals("John Doe",personRepository.findById(1).getName());
     }
+
+    @Test
+    void givenNothing_whenFetchPersons_thenReturnPersons(){
+        assertNotNull(personRepository);
+        assertEquals(1,personRepository.findAll().size());
+    }
+
+    @Test
+    void givenIdPerson_whenDeletePerson_thenReturnNothing(){
+        personRepository.deleteById(1);
+        assertEquals(0,personRepository.findAll().size());
+    }
+
+    @Test
+    void givenIdPerson_whenInsertPerson_thenReturnPersons(){
+        Person person = new Person();
+        person.setId(2);
+        person.setLocation("tizi");
+        person.setName("krimou");
+        personRepository.insertPerson(person);
+        assertEquals(2,personRepository.findAll().size());
+    }
+
 }
